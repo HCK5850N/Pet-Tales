ff76150ef7140d12af22053b9608f0b36563415b
Pet Tales <148808906+HCK5850N@users.noreply.github.com>
2023-10-26 19:06:03 -0300

    Update main.js

---
+++ main.js
@@ -0,0 +1,12 @@
+"use strict";
+
+function linkfacebook() {
+  window.open("https://www.facebook.com/FunnyPetTales");
+}
+
+function linkinstagram() {
+  window.open(
+    "https://www.instagram.com/funnypettales/?fbclid=IwAR3R3XEvgtC2riGmPSlDN9rtmIiGQnC1BXR-z-hUpeCw1gmUg47L6LJJnuE"
+  );
+}
+
